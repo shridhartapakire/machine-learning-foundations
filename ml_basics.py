2d100ac52ffa0e124a28beadc633048af48aa187
Shridhar <shridhartapakire4051@gmail.com>
2026-06-10 22:19:21 +0530

    ML Foundations: introduction to machine learning types

---
+++ ml_basics.py
@@ -0,0 +1,12 @@
+# Machine Learning Foundations - Day 1
+
+print("Machine Learning Basics")
+
+types = [
+    "Supervised Learning",
+    "Unsupervised Learning",
+    "Reinforcement Learning"
+]
+
+for t in types:
+    print("-", t)
