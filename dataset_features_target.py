91a6c208a5e136556f8d20e5f34f434d23a44175
Shridhar <shridhartapakire4051@gmail.com>
2026-06-12 21:42:13 +0530

    ML Foundations: dataset features target variable concepts

---
+++ dataset_features_target.py
@@ -0,0 +1,16 @@
+# ML Foundations - Day 2
+
+# Example dataset
+
+size = [1000, 1500, 2000]
+bedrooms = [2, 3, 4]
+
+# Target variable
+price = [5000000, 7500000, 10000000]
+
+print("Features:")
+print("Size:", size)
+print("Bedrooms:", bedrooms)
+
+print("\nTarget:")
+print("Price:", price)
