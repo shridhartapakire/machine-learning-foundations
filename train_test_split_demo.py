2889a3411077b544064b74e1bd13b6ee02634bfb
Shridhar <shridhartapakire4051@gmail.com>
2026-06-15 21:32:17 +0530

    ML Foundations: training and testing data concepts

---
+++ train_test_split_demo.py
@@ -0,0 +1,13 @@
+# ML Foundations - Day 3
+
+# Example dataset
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 80% training, 20% testing
+
+train_data = data[:8]
+test_data = data[8:]
+
+print("Training Data:", train_data)
+print("Testing Data:", test_data)
